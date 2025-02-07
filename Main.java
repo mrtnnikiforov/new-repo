06eba985d9d5080c4218dee20dda291f7b6494bb
mrtnnikiforov <mrtn.nkfrv@gmail.com>
2025-02-07 14:26:52 +0200

    new lines

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("challenge 123");
         System.out.println("New line");
+        System.out.println("Another line");
+        System.out.println("Another new line");
+
     }
 }
